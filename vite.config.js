618e0f951693668defa8c0237ce3a6fc48b6275d
Tharuka Dilshan <tharukad92@gmail.com>
2026-05-05 17:31:31 +0100

    3rd

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/demo-travel/', // IMPORTANT: If your GitHub repo name is NOT demo-travel, change this!
 })
